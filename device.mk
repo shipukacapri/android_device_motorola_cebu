d551e2fe54c91809d3ec49fe80cc9bff57c537a1
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-11 12:55:10 +0530

    add motocam

--- device.mk
+++ device.mk
@@ -21,6 +21,12 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1640
 TARGET_SCREEN_WIDTH := 720
 
+# Moto Camera 4
+TARGET_MOTCAMERA3 := cebu
+TARGET_USES_MOTCAMERA3 := true
+
+$(call inherit-product, vendor/motorola/MotCamera4-bengal/motcamera4.mk)
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
