632311d6b6c7494e58681c79b5c50a685e9c16a2
shipukacapri <242039583+shipukacapri@users.noreply.github.com>
2026-08-12 08:40:46 +0530

    cebu: InfinityX bringup, Switch to erofs, Add Motocam

--- infinity_cebu.mk
+++ infinity_cebu.mk
@@ -10,11 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/cebu/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Infinity Flags
+INFINITY_MAINTAINER := Shipu
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_cebu
+PRODUCT_NAME := infinity_cebu
 PRODUCT_DEVICE := cebu
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(9) power
